5f5b523d447a3282d5a2c4464c46a5c430bc1c29
Omar Essam <omar161157@eng.psu.edu.eg>
2023-08-16 02:32:29 +0300

    new commit

---
+++ Algorithms/Number theory/rules.cpp
@@ -0,0 +1,89 @@
+
+
+// 1 to n
+s = n*(n + 1) / 2;
+
+// 1 to n^2
+[n(n+1)(2n+1)] / 6
+
+// 1 to n^3
+(n(n+1)/2)^2 
+
+// sum of odds from 1 to n	
+((n + 1)/2)^2)
+
+
+inverse 1 to n sum
+// root(2s +0.25) - 0.5
+
+
+
+//~ Divisibility Rule of 1
+//~ Every number is divisible by 1. Divisibility rule for 1 doesn’t have any condition. Any number divided by 1 will give the number itself, irrespective of how large the number is. For example, 3 is divisible by 1 and 3000 is also divisible by 1 completely.
+
+//~ Divisibility Rule of 2
+//~ If a number is even or a number whose last digit is an even number i.e.  2,4,6,8 including 0, it is always completely divisible by 2.
+
+
+
+//~ Divisibility Rules for 3
+//~ Divisibility rule for 3 states that a number is completely divisible by 3 if the sum of its digits is divisible by 3.
+
+
+//~ Divisibility Rule of 4
+//~ If the last two digits of a number are divisible by 4, then that number is a multiple of 4 and is divisible by 4 completely.
+
+//~ Example: Take the number 2308. Consider the last two digits i.e.  08. As 08 is divisible by 4, the original number 2308 is also divisible by 4.
+
+//~ Divisibility Rule of 5
+//~ Numbers, which last with digits, 0 or 5 are always divisible by 5.
+
+//~ Divisibility Rule of 6
+//~ Numbers which are divisible by both 2 and 3 are divisible by 6. That is, if the last digit of the given number is even and the sum of its digits is a multiple of 3, then the given number is also a multiple of 6.
+
+//~ Divisibility Rules for 7
+//~ The rule for divisibility by 7 is a bit complicated which can be understood by the steps given below:
+
+//~ Divisibility rule of 7
+
+//~ From the rule stated remove 3 from the number and double it, which becomes 6.
+//~ Remaining number becomes 107, so 107-6 = 101.
+//~ Repeating the process one more time, we have 1 x 2 = 2.
+//~ Remaining number 10 – 2 = 8.
+//~ As 8 is not divisible by 7, hence the number 1073 is not divisible by 7.
+
+//~ Divisibility Rule of 8
+//~ If the last three digits of a number are divisible by 8, then the number is completely divisible by 8.
+
+//~ Example: Take number 24344. Consider the last two digits i.e.  344. As 344 is divisible by 8, the original number 24344 is also divisible by 8.
+
+//~ Divisibility Rule of 9
+//~ The rule for divisibility by 9 is similar to divisibility rule for 3. That is, if the sum of digits of the number is divisible by 9, then the number itself is divisible by 9.
+
+
+//~ Divisibility Rule of 10
+//~ Divisibility rule for 10 states that any number whose last digit is 0, is divisible by 10.
+
+//~ Divisibility Rules for 11
+//~ If the difference of the sum of alternative digits of a number is divisible by 11, then that number is divisible by 11 completely.
+
+//~ i.e., Sum of digits in odd places – Sum of digits in even places = 0 or a multiple of 11
+
+//~ In order to check whether a number like 2143 is divisible by 11, below is the following procedure.
+
+//~ Group the alternative digits i.e. digits which are in odd places together and digits in even places together. Here 24 and 13 are two groups.
+//~ Take the sum of the digits of each group i.e. 2+4=6 and 1+3= 4
+//~ Now find the difference of the sums; 6-4=2
+//~ If the difference is divisible by 11, then the original number is also divisible by 11. Here 2 is the difference which is not divisible by 11.
+//~ Therefore, 2143 is not divisible by 11.
+//~ A few more conditions are there to test the divisibility of a number by 11. They are explained here with the help of examples:
+
+//~ If the number of digits of a number is even, then add the first digit and subtract the last digit from the rest of the number. 
+
+
+//~ Divisibility Rule of 12
+//~ If the number is divisible by both 3 and 4, then the number is divisible by 12 exactly. 
+
+
+//~ Divisibility Rules for 13
+//~ For any given number, to check if it is divisible by 13, we have to add four times of the last digit of the number to the remaining number and repeat the process until you get a two-digit number.  Now check if that two-digit number is divisible by 13 or not. If it is divisible, then the given number is divisible by 13.
